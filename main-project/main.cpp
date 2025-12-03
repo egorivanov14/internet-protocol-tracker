8b0d4d1c26d484e92b945c234cf60d34d053332f
Ivanov Egor <max203383@gmail.com>
2025-12-03 20:32:28 +0300

    Создана заготовка проекта

---
+++ main-project/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    setlocale(LC_ALL, "Russian");
+    cout << "Протокол работы в Интернет" << endl;
+    cout << "Вариант: Протокол использования сети" << endl;
+    cout << "Автор: Иванов Егор" << endl;
+    return 0;
+}
